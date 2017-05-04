4b688e8e7dce743a0f8a40c49474a70e3ea5c6ee
Slawomir Skowron <szibis@gmail.com>
2017-05-04 16:40:48 +0200

    Add better output for test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
+NOC='\033[0m'
+SECRETS_REPO="https://github.com/futuresimple/helm-secrets"
+
 test_encryption() {
 result=$(cat < "${secret}" | grep -Ec "(40B6FAEC80FD467E3FE9421019F6A67BB1B8DDBE|4434EA5D05F10F59D0DF7399AF1D073646ED4927)")
 if [ "${result}" -eq 2 ] && [ "${secret}" == "./example/helm_vars/secrets.yaml" ];
 then
-    echo "[OK] File properly encrypted"
+    echo -e "${GREEN}[OK]${NOC} File properly encrypted"
 elif [ "${result}" -eq 1 ] && [ "${secret}" != "./example/helm_vars/secrets.yaml" ];
 then
-    echo "[OK] File properly encrypted"
+    echo -e "${GREEN}[OK]${NOC} File properly encrypted"
 else
-    echo "[FAIL] ${secret} Not encrypted properly"
+    echo -e "${RED}[FAIL]${NOC} ${secret} Not encrypted properly"
     exit 1
 fi
 }
@@ -18,9 +25,9 @@ test_view() {
 result_view=$(helm-wrapper secrets view "${secret}" | grep -Ec "(40B6FAEC80FD467E3FE9421019F6A67BB1B8DDBE|4434EA5D05F10F59D0DF7399AF1D073646ED4927)")
 if [ "${result_view}" -gt 0 ];
 then
-    echo "[FAIL] Decryption failed"
+    echo -e "${RED}[FAIL]${NOC} Decryption failed"
 else
-    echo "[OK] File decrypted"
+    echo -e "${GREEN}[OK]${NOC} File decrypted"
 fi
 }
 
@@ -30,62 +37,62 @@ then
     result_dec=$(cat < "${secret}.dec" | grep -Ec "(40B6FAEC80FD467E3FE9421019F6A67BB1B8DDBE|4434EA5D05F10F59D0DF7399AF1D073646ED4927)")
     if [ "${result_dec}" -gt 0 ];
     then
-        echo "[FAIL] Decryption failed"
+        echo -e "${RED}[FAIL]${NOC} Decryption failed"
     else
-        echo "[OK] File decrypted"
+        echo -e "${GREEN}[OK]${NOC} File decrypted"
     fi
 else
-    echo "[FAIL] ${secret}.dec not exist"
+    echo -e "${RED}[FAIL]${NOC} ${secret}.dec not exist"
     exit 1
 fi
 }
 
 test_helm_secrets() {
-echo "+++ Testing ${secret}"
-echo "+++ Encrypt and Test"
+echo -e "${YELLOW}+++${NOC} ${BLUE}Testing ${secret}${NOC}"
+echo -e "${YELLOW}+++${NOC} Encrypt and Test"
 helm-wrapper secrets enc "${secret}" 2>&1 >/dev/null && \
 test_encryption "${secret}"
-echo "+++ View encrypted test"
+echo -e "${YELLOW}+++${NOC} View encrypted test"
 test_view "${secret}"
-echo "+++ Decrypt"
+echo -e "${YELLOW}+++${NOC} Decrypt"
 helm-wrapper secrets dec "${secret}" 2>&1 >/dev/null && \
 test_decrypt "${secret}" && \
 mv "${secret}.dec" "${secret}"
-echo "+++ Once again Encrypt and Test"
+echo -e "${YELLOW}+++${NOC} Once again Encrypt and Test"
 helm-wrapper secrets enc "${secret}" 2>&1 >/dev/null && \
 test_encryption "${secret}"
 }
 
-echo "+++ Installing helm-secrets plugin"
+echo -e "${YELLOW}+++${NOC} Installing helm-secrets plugin"
 if [ "$(helm plugin list | tail -n1 | cut -d ' ' -f 1 | grep -c "secrets")" -eq 1 ];
 then
-    echo "[OK] helm-ecrets plugin installed"
+    echo -e "${GREEN}[OK]${NOC} helm-ecrets plugin installed"
 else
-    helm plugin install https://github.com/futuresimple/helm-secrets 2>/dev/null
-    echo "[FAIL] No helm-secrets plugin aboting"
+    helm plugin install "${SECRETS_REPO}" 2>/dev/null
+    echo -e "${RED}[FAIL]${NOC} No helm-secrets plugin aboting"
     exit 1
 fi
 
 echo ""
 if [ -x "$(command -v gpg --version)" ];
 then
-    echo "+++ Importing private pgp key for projectx"
+    echo -e "${YELLOW}+++${NOC} Importing private pgp key for projectx"
     gpg --import example/pgp/projectx.asc
     echo ""
-    echo "+++ Importing private pgp key for projectx"
+    echo -e "${YELLOW}+++${NOC} Importing private pgp key for projectx"
     gpg --import example/pgp/projecty.asc
     echo ""
 else
-    echo "Install gpg"
+    echo -e "${RED}[FAIL]${NOC} Install gpg"
     exit 1
 fi
 
-echo "+++ Show helm_vars tree from example"
+echo -e "${YELLOW}+++${NOC} Show helm_vars tree from example"
 if [ -x "$(command -v tree --version)" ];
 then
     tree -Ca example/helm_vars/
 else
-    echo "Install tree command"
+    echo -e "${RED}[FAIL]${NOC} Install tree command"
     exit 1
 fi
 
