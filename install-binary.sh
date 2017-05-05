1c864ec4c6337723841b2bb12606a6abef1df710
Slawomir Skowron <szibis@gmail.com>
2017-05-05 12:02:19 +0200

    Bump plugin version and ad some colors in install for more visibility

--- install-binary.sh
+++ install-binary.sh
@@ -2,6 +2,12 @@
 
 SOPS_VERSION="2.0.9"
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+#BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
+NOC='\033[0m'
+
 HELM_WRAPPER="/usr/local/bin/helm-wrapper"
 
 SOPS_DEB_URL="https://go.mozilla.org/sops/dist/sops_${SOPS_VERSION}_amd64.deb"
@@ -25,20 +31,26 @@ then
         then
             sudo dpkg -i /tmp/sops.deb;
         else
-            echo "Wrong SHA256"
+            echo -e "${RED}Wrong SHA256${NOC}"
         fi
     fi
 else
-    echo "No SOPS package available"
+    echo -e "${RED}No SOPS package available${NOC}"
     exit 1
 fi
 
 ### git diff config
-git config diff.sopsdiffer.textconv "sops -d"
+if [ -x "$(command -v git --version)" ];
+then
+    git config diff.sopsdiffer.textconv "sops -d"
+else
+    echo -e "${RED}[FAIL]${NOC} Install git command"
+    exit 1
+fi
 
 ### Helm-secrets wrapper for helm command with auto decryption and cleanup on the fly
 echo ""
-echo -n "Helm-secrets wrapper for helm binary: "
+echo -ne "${YELLOW}*${NOC} Helm-secrets wrapper for helm binary: "
 if [ -f "${HOME}/.helm/plugins/helm-secrets.git/wrapper.sh" ];
 then
     ln -s "${HOME}"/.helm/plugins/helm-secrets.git/wrapper.sh ${HELM_WRAPPER} 2>/dev/null
@@ -49,7 +61,7 @@ fi
 
 if [ -f ${HELM_WRAPPER} ];
 then
-    echo ${HELM_WRAPPER}
+    echo -e "${GREEN}${HELM_WRAPPER}${NOC}"
 else
-    echo "No ${HELM_WRAPPER} installed"
+    echo -e "${RED}No ${HELM_WRAPPER} installed${NOC}"
 fi
