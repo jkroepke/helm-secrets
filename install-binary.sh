d762a6d9d79d998fc263a7ad5cdb3cb4ae057b9d
mattclegg <cleggmatt@gmail.com>
2017-10-18 17:21:58 +0200

    UPDATE sha to be correct for 2.0.9

--- install-binary.sh
+++ install-binary.sh
@@ -4,7 +4,7 @@ set -ueo pipefail
 
 SOPS_VERSION="2.0.9"
 SOPS_DEB_URL="https://go.mozilla.org/sops/dist/sops_${SOPS_VERSION}_amd64.deb"
-SOPS_DEB_SHA="fdc3559d6f16a54ec1d54d4a0aa1d7a3d273207ec78a37f9869dd2a1b32f5292"
+SOPS_DEB_SHA="444c689ebab45150e751465be392bfbacf1e319fdc7e98db979e5dbf675038b4"
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
