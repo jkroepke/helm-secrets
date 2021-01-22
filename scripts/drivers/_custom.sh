9d578219878451e69feb1b78f1bb040a769fac98
Jan-Otto Kröpke <github@jkroepke.de>
2021-01-22 22:51:42 +0100

    detect gnu compatible sed on any system (#72)

--- scripts/drivers/_custom.sh
+++ scripts/drivers/_custom.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env sh
 
-_sed_i() {
-    # MacOS syntax is different for in-place
-    if [ "$(uname)" = "Darwin" ]; then
-        sed -i "" "$@"
-    else
-        sed -i "$@"
-    fi
-}
+# MacOS syntax is different for in-place
+# https://unix.stackexchange.com/a/92907/433641
+case $(sed --help 2>&1) in
+*BusyBox* | *GNU*) _sed_i() { sed -i "$@"; } ;;
+*) _sed_i() { sed -i '' "$@"; } ;;
+esac
 
 _regex_escape() {
     # This is a function because dealing with quotes is a pain.
