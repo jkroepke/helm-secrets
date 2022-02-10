92738569e7f453cb193ddece5f88a0a86a4ac2b6
Jan-Otto Kröpke <mail@jkroepke.de>
2022-02-10 17:24:01 +0100

    debug

--- scripts/lib/file.sh
+++ scripts/lib/file.sh
@@ -38,6 +38,8 @@ _file_exists() {
 _file_get() {
     file_type=$(_file_get_protocol "${1}")
 
+    set -x
+
     if [ "${file_type}" = "local" ]; then
         if [ "${VALUES_ALLOW_SYMLINKS}" = "false" ] && [ -L "${1}" ]; then
             error "Values file '%s' is a symlink. Symlinks are not allowed." "${1}"
