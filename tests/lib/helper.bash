3a6daed9e0bc035e593acef742a60b2d9f4abcc3
Jan-Otto Kröpke <mail@jkroepke.de>
2022-02-24 01:13:36 +0100

    run tests in wsl

--- tests/lib/helper.bash
+++ tests/lib/helper.bash
@@ -97,7 +97,6 @@ initiate() {
 }
 
 setup() {
-    set -x
     REAL_HOME="${HOME}"
     # shellcheck disable=SC2153
     HOME="$(_home_dir)"
@@ -204,8 +203,6 @@ EzAA
     export _TEST_global_secret=global_bar
     export _TEST_SERVICE_PORT=81
     export _TEST_SOME_SERVICE_PORT=83
-
-    set +x
 }
 
 teardown() {
