040fef0f137234969d808ab4b2563f4c4bf8e619
Jan-Otto Kröpke <mail@jkroepke.de>
2022-07-31 21:48:01 +0200

    enable more tests on WSL

--- scripts/wrapper/sh.cmd
+++ scripts/wrapper/sh.cmd
@@ -104,25 +104,28 @@ exit /b %ERRORLEVEL%
 
 
 :WSL
-SET argCount=0
-for %%x in (%*) do (
-    SET /A argCount+=1
-    SET "STR1=%%x"
-    IF NOT "!STR1:\=!"=="!STR1!" (
-        :: CMD output to variable - https://stackoverflow.com/a/6362922/8087167
-        FOR /F "tokens=* USEBACKQ" %%F IN (`wsl wslpath %STR1:\=/%`) DO (
-            SET "argVec[!argCount!]=%%~F"
-        )
-    ) else (
-        SET "argVec[!argCount!]=%%~x"
+:: Use WSL, but convert all paths (script + arguments) to wsl paths
+SET ARGS=
+
+:: Loop through all parameters - https://stackoverflow.com/a/34019557/8087167
+:WSLPATHLOOP
+if [%1]==[] goto WSLPATHENDLOOP
+
+:: IF string contains string - https://stackoverflow.com/a/7006016/8087167
+SET STR1=%1
+if not [x%STR1:\=%]==[x%STR1%] (
+    :: CMD output to variable - https://stackoverflow.com/a/6362922/8087167
+    FOR /F "tokens=* USEBACKQ" %%F IN (`wsl wslpath %STR1:\=/%`) DO (
+        SET WSLPATH="%%F"
     )
+) else (
+    SET WSLPATH=%STR1%
 )
+SET ARGS=%ARGS% %WSLPATH%
 
-echo Number of processed arguments: %argCount%
-
-for /L %%i in (1,1,%argCount%) do echo %%i- "!argVec[%%i]!"
-
-exit /b 1
+shift
+goto WSLPATHLOOP
+:WSLPATHENDLOOP
 
 :: WSL needs .exe suffix for windows binary. Define path only if exists in windows PATH
 IF NOT DEFINED HELM_BIN (
