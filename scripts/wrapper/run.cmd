be7e86a4801f0240128c57abc80897ac9a1079f5
Jan-Otto Kröpke <mail@jkroepke.de>
2022-08-02 21:37:58 +0200

    Update run.cmd

--- scripts/wrapper/run.cmd
+++ scripts/wrapper/run.cmd
@@ -1,17 +1,5 @@
 :<<BATCH
     @echo off
-    setlocal enabledelayedexpansion
-
-    set argCount=0
-    for %%x in (%*) do (
-       set /A argCount+=1
-       set "argVec[!argCount!]=%%~x"
-    )
-
-    echo Number of processed arguments: %argCount%
-
-    for /L %%i in (1,1,%argCount%) do echo %%i- "!argVec[%%i]!"
-
     "%~dp0\sh.cmd" "%HELM_PLUGIN_DIR%"/%*
     exit /b %errorlevel%
 BATCH
