e6095bc7f7ac7a19b01419aa391ee7df67b404de
Jan-Otto Kröpke <mail@jkroepke.de>
2022-09-24 19:58:36 +0200

    Native support plugin downloaders on Windows

--- scripts/wrapper/run.cmd
+++ scripts/wrapper/run.cmd
@@ -1,13 +1,15 @@
-:<<BATCH
-    @echo off
-    "%~dp0\sh.cmd" "%HELM_PLUGIN_DIR%"/%*
-    exit /b %errorlevel%
-BATCH
-
+#!/usr/bin/env sh
+@ 2>/dev/null # 2>nul & echo off & goto BOF
+:
 cmd="$1"
 shift
 "$HELM_PLUGIN_DIR/$cmd" "$@"
 exit $?
+
+:BOF
+@echo off
+"%~dp0\sh.cmd" "%HELM_PLUGIN_DIR%"/%*
+exit /b %errorlevel%
 
 :: .bat bash hybrid script
 :: https://stackoverflow.com/a/17510832/8087167
