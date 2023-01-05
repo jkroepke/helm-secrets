04976bc9b9bf933e47643276dcb025b1835222e7
Jan-Otto Kröpke <mail@jkroepke.de>
2023-01-05 23:42:04 +0100

    Add --debug

--- scripts/commands/post-renderer.sh
+++ scripts/commands/post-renderer.sh
@@ -6,6 +6,6 @@ post_renderer() {
     if [ "${EVALUATE_TEMPLATES_DECODE_SECRETS}" = "true" ]; then
         _vals ksdecode -f - | backend_decrypt_file "yaml" "-"
     else
-        tee /dev/stderr | backend_decrypt_file "yaml" "-"
+        cat -
     fi
 }
