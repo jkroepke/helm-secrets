5b459331b9a7fcd21707b07f919b7fe0558282e1
Jan-Otto Kröpke <mail@jkroepke.de>
2023-01-07 18:43:56 +0100

    Add ps wrapper

--- scripts/wrapper/sh.ps1
+++ scripts/wrapper/sh.ps1
@@ -33,7 +33,52 @@ function shellWsl {
         [string[]][Parameter(Position=1, ValueFromRemainingArguments)] $args
     )
     process {
-        $wslArgs = @("bash")
+        if ($null -ne $env:HELM_BIN -and $null -ne $env:HELM_SECRETS_HELM_PATH) {
+            if ((which helm.exe) -ne $null) {
+                $env:HELM_SECRETS_HELM_PATH = "helm.exe"
+            }
+        }
+
+        if ($null -ne $env:HELM_SECRETS_SOPS_PATH) {
+            if ((which sops.exe) -ne $null) {
+                $env:HELM_SECRETS_SOPS_PATH = "sops.exe"
+            }
+        }
+
+        if ($null -ne $env:HELM_SECRETS_VALS_PATH) {
+            if ((which vals.exe) -ne $null) {
+                $env:HELM_SECRETS_VALS_PATH = "vals.exe"
+            }
+        }
+
+        if ($null -ne $env:HELM_SECRETS_CURL_PATH) {
+            if ((which curl.exe) -ne $null) {
+                $env:HELM_SECRETS_CURL_PATH = "curl.exe"
+            }
+        }
+
+        $env:WSLENV += ':SOPS_AGE_KEY:SOPS_AGE_KEY_FILE'
+        $env:WSLENV += ':HELM_SECRETS_DEC_SUFFIX'
+        $env:WSLENV += ':HELM_SECRETS_QUIET'
+        $env:WSLENV += ':HELM_SECRETS_BACKEND'
+        $env:WSLENV += ':HELM_SECRETS_BACKEND_ARGS'
+        $env:WSLENV += ':HELM_SECRETS_DEC_DIR'
+        $env:WSLENV += ':HELM_SECRETS_URL_VARIABLE_EXPANSION'
+        $env:WSLENV += ':HELM_DEBUG'
+        $env:WSLENV += ':HELM_SECRETS_EVALUATE_TEMPLATES'
+        $env:WSLENV += ':HELM_SECRETS_EVALUATE_TEMPLATES_DECODE_SECRETS'
+
+        $env:WSLENV += if ($env:HELM_BIN -match "\\") {":HELM_PLUGIN_DIR/p"} else {":HELM_PLUGIN_DIR"}
+        $env:WSLENV += if ($env:HELM_PLUGIN_DIR -match "\\") {":HELM_PLUGIN_DIR/p"} else {":HELM_PLUGIN_DIR"}
+        $env:WSLENV += if ($env:HELM_SECRETS_HELM_PATH -match "\\") {":HELM_SECRETS_HELM_PATH/p"} else {":HELM_SECRETS_HELM_PATH"}
+        $env:WSLENV += if ($env:HELM_SECRETS_SOPS_PATH -match "\\") {":HELM_SECRETS_SOPS_PATH/p"} else {":HELM_SECRETS_SOPS_PATH"}
+        $env:WSLENV += if ($env:HELM_SECRETS_VALS_PATH -match "\\") {":HELM_SECRETS_VALS_PATH/p"} else {":HELM_SECRETS_VALS_PATH"}
+        $env:WSLENV += if ($env:HELM_SECRETS_CURL_PATH -match "\\") {":HELM_SECRETS_CURL_PATH/p"} else {":HELM_SECRETS_VALS_PATH"}
+
+        if ($args[0] -match "\\") {
+            $args[0] = wsl wslpath "$(arg[0])"
+        }
+
         $proc = Start-Process -FilePath "wsl.exe" -ArgumentList $args -NoNewWindow -Wait -PassThru
         $proc.WaitForExit();
         exit $proc.ExitCode
@@ -52,20 +97,15 @@ if ($null -ne $env:HELM_SECRETS_WINDOWS_SHELL) {
     shellEnv $env:HELM_SECRETS_WINDOWS_SHELL $args
 }
 
-if (Test-Path -Path "$($env:ProgramFiles)\Git\bin\bash.exe") {
-    shellWindowsNative "$($env:ProgramFiles)\Git\bin\bash.exe" $args
-}
-
-if (Test-Path -Path "$($env:UserProfile)\scoop\shims\bash.exe") {
-    shellWindowsNative "$($env:UserProfile)\scoop\shims\bash.exe" $args
-}
-
-if (Test-Path -Path "$($env:UserProfile)\scoop\shims\sh.exe") {
-    shellWindowsNative "$($env:UserProfile)\scoop\shims\sh.exe" $args
-}
-
-if (Test-Path -Path "${$env:ProgramFiles(x86)}\Git\bin\bash.exe") {
-    shellWindowsNative "${$env:ProgramFiles(x86)}\Git\bin\bash.exe" $args
+@(
+    ("$($env:ProgramFiles)\Git\bin\bash.exe"),
+    ("${$env:ProgramFiles(x86)}\Git\bin\bash.exe"),
+    ("$($env:UserProfile)\scoop\shims\bash.exe"),
+    ("$($env:UserProfile)\scoop\shims\sh.exe")
+) | % {
+    if (Test-Path -Path "$_") {
+        shellWindowsNative "$_" $args
+    }
 }
 
 $gitPath = (which git.exe)
