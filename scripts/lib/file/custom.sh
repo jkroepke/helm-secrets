72d0538a63cacf9f35fb1fb4453f63b2a745d7cc
Jan-Otto Kröpke <mail@jkroepke.de>
2023-03-07 23:40:42 +0100

    avoid tmp files

--- scripts/lib/file/custom.sh
+++ scripts/lib/file/custom.sh
@@ -7,14 +7,12 @@ _file_custom_exists() {
 }
 
 _file_custom_get() {
-    _tmp_file=$(_mktemp)
     GETTER_CHART_PATH="$(_helm_winpath "${SCRIPT_DIR}/lib/file/helm-values-getter")"
     VALUES="$(_helm_winpath "${1}")"
 
-    if ! "${HELM_BIN}" template "${GETTER_CHART_PATH}" --set-file "content=${VALUES}" >"${_tmp_file}"; then
+    if ! VALUES_CONTENT=$("${HELM_BIN}" template "${GETTER_CHART_PATH}" --set-file "content=${VALUES}"); then
         exit 1
     fi
 
-    _sed_i -e '1,3d' -e 's/^  //g' "${_tmp_file}"
-    printf '%s' "${_tmp_file}"
+    printf '%s' "${VALUES_CONTENT}" | sed -e '1,3d' -e 's/^  //g'
 }
