099d0cf7131d1997425dd46ac8e806c6552fe2b4
Jan-Otto Kröpke <mail@jkroepke.de>
2023-03-07 23:49:13 +0100

    revert "avoid tmp files"

--- scripts/lib/file/custom.sh
+++ scripts/lib/file/custom.sh
@@ -7,12 +7,14 @@ _file_custom_exists() {
 }
 
 _file_custom_get() {
+    _tmp_file=$(_mktemp)
     GETTER_CHART_PATH="$(_helm_winpath "${SCRIPT_DIR}/lib/file/helm-values-getter")"
     VALUES="$(_helm_winpath "${1}")"
 
-    if ! VALUES_CONTENT=$("${HELM_BIN}" template "${GETTER_CHART_PATH}" --set-file "content=${VALUES}"); then
+    if ! "${HELM_BIN}" template "${GETTER_CHART_PATH}" --set-file "content=${VALUES}" >"${_tmp_file}"; then
         exit 1
     fi
 
-    printf '%s' "${VALUES_CONTENT}" | sed -e '1,3d' -e 's/^  //g'
+    _sed_i -e '1,3d' -e 's/^  //g'  "${_tmp_file}"
+    printf '%s' "${_tmp_file}"
 }
