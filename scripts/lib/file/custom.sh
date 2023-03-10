d28fc8db9ad61440e37f8448d1ab267beb08bb5e
Jan-Otto Kröpke <mail@jkroepke.de>
2023-03-10 21:33:39 +0100

    --debug for windows

--- scripts/lib/file/custom.sh
+++ scripts/lib/file/custom.sh
@@ -9,9 +9,8 @@ _file_custom_exists() {
 _file_custom_get() {
     _tmp_file=$(_mktemp)
     GETTER_CHART_PATH="$(_helm_winpath "${SCRIPT_DIR}/lib/file/helm-values-getter")"
-    VALUES="$(_helm_winpath "${1}")"
 
-    if ! CONTENT="$("${HELM_BIN}" template "${GETTER_CHART_PATH}" --set-file "content=${VALUES}")"; then
+    if ! CONTENT="$("${HELM_BIN}" template "${GETTER_CHART_PATH}" --set-file "content=${1}")"; then
         fatal "helm template command errored on value '%s'" "${1}"
     fi
 
