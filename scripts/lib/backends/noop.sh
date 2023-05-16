23f176abc863ab1398bbf46962e73cd4972cadf8
Jan-Otto Kröpke <mail@jkroepke.de>
2023-05-16 19:23:18 +0200

    noop driver should do nothing by default
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- scripts/lib/backends/noop.sh
+++ scripts/lib/backends/noop.sh
@@ -1,41 +1,7 @@
 #!/usr/bin/env sh
 
 _noop_backend_is_file_encrypted() {
-    true
-}
-
-_noop_backend_encrypt_file() {
-    # shellcheck disable=SC2034
-    type="${1}"
-    input="${2}"
-    output="${3}"
-
-    if [ "${input}" = "${output}" ]; then
-        # encrypt in-place
-        true
-    else
-        cat "${input}" >"${output}"
-    fi
-}
-
-_noop_backend_decrypt_file() {
-    # shellcheck disable=SC2034
-    type="${1}"
-    input="${2}"
-    # if omit then output to stdout
-    output="${3:-}"
-
-    if [ "${input}" = "${output}" ]; then
-        :
-    elif [ "${output}" = "" ]; then
-        cat "${input}"
-    else
-        cat "${input}" >"${output}"
-    fi
-}
-
-_noop_backend_decrypt_literal() {
-    printf '%s' "${1}"
+    false
 }
 
 _noop_backend_edit_file() {
