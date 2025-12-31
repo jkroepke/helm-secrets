5a68fd7b0338defca8c3d20f440416ba13dd294e
Jan-Otto Kröpke <mail@jkroepke.de>
2025-12-31 08:21:28 +0100

    fix: noop backend not support encrypted files (#680)

--- scripts/lib/backends/noop.sh
+++ scripts/lib/backends/noop.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env sh
 
 _noop_backend_is_file_encrypted() {
-    false
+    true
+}
+
+_noop_backend_is_encrypted() {
+    true
 }
 
 _noop_backend_edit_file() {
@@ -10,4 +14,24 @@ _noop_backend_edit_file() {
     input="${2}"
 
     "${EDITOR:-vi}" "${input}"
+}
+
+_noop_backend_encrypt_file() {
+    fatal "Encrypting files is not supported!"
+}
+
+_noop_backend_decrypt_file() {
+    input="${2}"
+    # if omit then output to stdout
+    output="${3:-}"
+
+    if [ "${input}" = "${output}" ]; then
+        :
+    elif [ "${input}" = "-" ]; then
+        cat
+    elif [ "${output}" = "" ]; then
+        cat <"${input}"
+    else
+        cp "${input}" "${output}"
+    fi
 }
